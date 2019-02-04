bf225c665054ec9c6c442f864128db4a0df26a66
Sarah Pai <pai.sarah89@gmail.com>
2019-02-04 14:38:48 -0500

    fetch to heroku link and update frame

--- src/actions/frames.jsx
+++ src/actions/frames.jsx
@@ -1,53 +1,5 @@
 import {  SELECT_FRAME } from '../reducers/types.js';
 
-// export const fetchHorizontalFrame = () => {
-// 	// takes the token in localStorage and finds out who it belongs to
-// 	return (dispatch) => {
-// 	  fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/frames`, {
-// 			method: 'GET',
-// 			headers: {
-// 				'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-// 			}
-// 		})
-// 		.then(response => response.json())
-// 		  .then((JSONResponse) => { 
-// 			console.log(JSONResponse)
-// 			  dispatch({ type: ADD_HORIZONTAL_FRAME, payload: JSONResponse[0].frame_horizontal })
-// 		  })
-// 	}
-// }
-
-// export const fetchVerticalFrame = () => {
-// 	return (dispatch) => {
-// 		fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/frames`, {
-// 			  method: 'GET',
-// 			  headers: {
-// 				  'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-// 			  }
-// 		  })
-// 		  .then(response => response.json())
-// 			.then((JSONResponse) => { 
-// 			  console.log(JSONResponse)
-// 				dispatch({ type: ADD_VERTICAL_FRAME, payload: JSONResponse[0].frame_vertical })
-// 			})
-// 	  }
-// }
-
-// export const fetchGifFrame = () => {
-// 	return (dispatch) => {
-// 		fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/frames`, {
-// 			  method: 'GET',
-// 			  headers: {
-// 				  'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-// 			  }
-// 		  })
-// 		  .then(response => response.json())
-// 			.then((JSONResponse) => { 
-// 			  console.log(JSONResponse)
-// 				dispatch({ type: ADD_GIF_FRAME, payload: JSONResponse[0].frame_gif })
-// 			})
-// 	  }
-// }
 
 export const selectFrame = (clickedFrame) => {
 	return (dispatch) => {
